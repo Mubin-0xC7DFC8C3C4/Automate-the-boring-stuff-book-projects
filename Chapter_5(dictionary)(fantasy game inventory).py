c2921dd445dec59726b26d05eb944d84f1837c94
Mubin-0xC7DFC8C3C4 <146631192+Mubin-0xC7DFC8C3C4@users.noreply.github.com>
2026-05-23 00:14:16 +0600

    feat:add inventory management & display functions

--- Chapter_5(dictionary)(fantasy game inventory).py
+++ Chapter_5(dictionary)(fantasy game inventory).py
@@ -1,12 +1,20 @@
 import ast
 
 def displayInventory(inventory):
-    print("Inventory: ")
+    print("Inventory:")
     item_total = 0
     for key, value in inventory.items():
         print(str(value) + " " + str(key))
-        item_total = item_total + value
+        item_total += value
     print("Total number of items: " + str(item_total))
 
-displayInventory(ast.literal_eval(input()))
+def addToInventory(inventory, addedItems):
+    for item in addedItems:
+        inventory[item] = inventory.get(item, 0) + 1
+    return inventory
 
+inv = ast.literal_eval(input())
+dragonLoot = ast.literal_eval(input("Enter your dragon loot list: "))
+
+inv = addToInventory(inv, dragonLoot)
+displayInventory(inv)
