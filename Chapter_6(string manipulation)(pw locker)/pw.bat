a2a56a586fa4e7337aff559ac952e3a55a0ff0aa
Mubin-0xC7DFC8C3C4 <146631192+Mubin-0xC7DFC8C3C4@users.noreply.github.com>
2026-05-24 18:29:51 +0600

    chore(config): add batch launcher

---
+++ Chapter_6(string manipulation)(pw locker)/pw.bat
@@ -0,0 +1,2 @@
+@"C:\Users\HTC\AppData\Local\Programs\Python\Python313\python.exe" "C:\Users\HTC\python_code\Automate the boring stuff book project\Chapter_6(string manipulation)(pw locker)\password_locker.py" %*
+@pause
